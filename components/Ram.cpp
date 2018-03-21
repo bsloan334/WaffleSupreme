bfd078f5632c0597e8bb54a497e0165fd9503605
rbearden <rbeard13@students.kennesaw.edu>
2018-03-21 18:39:00 -0400

    Update and rename ram.cpp to Ram.cpp
    All Allocate() functions now return an integer address
    indicating the offset where the data was stored
    [This is so the Loader and LongTerm can store the
    appropriate base addresses in Processes they store]

--- components/Ram.cpp
+++ components/Ram.cpp
@@ -1,22 +1,29 @@
 #include "stdafx.h"
 #include <string>
+
 #include "RAM.hpp"
 
 using namespace std;
 
-void RAM::Allocate(byte_t data, int index) {
+int RAM::Allocate(byte_t data, int index) {
+   int address = index;
+   
    if (size > index) {
       storage.at(index) = data;
    } else {
       throw "Invalid allocaion at: " + to_string(index) + ".";
+      address = -1;   // essentially NULL address is returned
    }
+   
+   return address;
 }
 
-void RAM::Allocate(instruction_t data, int index) {
+int RAM::Allocate(instruction_t data, int index) {
    byte_t temp = 0;
+   int address = -1;
 
 	temp = byte_t((data & 0xFF000000) >> (8*3));
-	Allocate(temp, index);
+	address = Allocate(temp, index);                // instr's address is location of first byte
 
 	temp = byte_t((data & 0x00FF0000) >> (8*2));
 	Allocate(temp, index + 1);
@@ -26,10 +33,12 @@ void RAM::Allocate(instruction_t data, int index) {
 
 	temp = byte_t((data & 0x000000FF) >> (8*0));
 	Allocate(temp, index + 3);
+    
+    return address;
 }
 
 instruction_t RAM::GetInstruction(int index) {
-   instruction_t instruct = 0;
+	instruction_t instruct = 0;
 	instruct |= ((instruction_t)storage[index+0]) << (8 * 3); // Shift 3 bytes
 	instruct |= ((instruction_t)storage[index+1]) << (8 * 2);
 	instruct |= ((instruction_t)storage[index+2]) << (8 * 1);
