150e8f74db9137668920696738b7ba074278341f
rbearden <rbeard13@students.kennesaw.edu>
2018-03-22 23:01:09 -0400

    Converted word size (4) to constant stored in "Types.hpp"

--- components/RAM.hpp
+++ components/RAM.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
 #include <vector>
+#include <iostream>
 
 #include "Types.hpp"
 
+/*** Changes made by Ruth...
+All Allocate() functions now return an integer address
+indicating the offset where the data was stored
+[This is so the Loader and LongTerm can store the
+appropriate base addresses in Processes they store]
+
+******/
+
 //This makes the ram byte-addressable
-#define DEAFULT_RAM 1024 * 4
+#define DEAFULT_RAM 1024 * WORD
 
 using namespace std;
 
@@ -25,5 +34,4 @@ class RAM {
       instruction_t GetInstruction(int index);
 
       string GetStatus();
-
 };
