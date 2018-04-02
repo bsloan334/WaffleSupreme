864ae45b1ccfbef1a19b3f0959b273f5dfe98f48
rbearden <rbeard13@students.kennesaw.edu>
2018-04-01 20:26:45 -0400

    Update Dispatcher.cpp

--- components/Dispatcher.cpp
+++ components/Dispatcher.cpp
@@ -1,10 +1,10 @@
-#pragma once
-
 #include "Dispatcher.hpp"
 
 using namespace std;
 
 void Dispatcher::LoadProcessToCPU(Process* p, CPU* targetCPU)
 {
+	p->SetState(RUNNING);
 	targetCPU->RunProcess(p);
+	// print process output here?
 }
