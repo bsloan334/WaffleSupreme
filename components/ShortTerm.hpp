6ba4a2b9dbd8ded9a1903753cca370d556155e67
rbearden <rbeard13@students.kennesaw.edu>
2018-04-01 20:37:04 -0400

    Update ShortTerm.hpp

--- components/ShortTerm.hpp
+++ components/ShortTerm.hpp
@@ -1,21 +1,25 @@
 #pragma once
+
 #include <queue>
+#include <cstring>
+
 #include "Process.hpp"
+#include "LongTerm.hpp"
+#include "Dispatcher.hpp"
+#include "CPU.hpp"
 
 using namespace std;
 
 class ShortTerm{
    public:
-      
-
-      priority_queue<Process*, deque<Process*>> zeQueue;
-
-      ShortTerm();
-      ~ShortTerm();
-
-      Process* GetProcess();
-      void AddProcess(Process* process);
-      void RemoveProcess();
-      int Size();
-      bool IsEmpty() { return zeQueue.empty; };
+	   ShortTerm(queue<Process*>* zeQ, LongTerm* sched, CPU* target) :
+		   zeQueue(zeQ), scheduler(sched), targetCPU(target) {}
+	  bool RunNextProcess();
+   
+   private:
+	   LongTerm* scheduler;
+	   CPU* targetCPU;
+	   queue<Process*>* zeQueue;
+	   int cpuID;
+	   string outFileName = "JobOutputFile.txt";
 };
