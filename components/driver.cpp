f3d32e07297d9aef57d9dbd666fd1f7a93cd0329
rbearden <rbeard13@students.kennesaw.edu>
2018-04-01 20:25:36 -0400

    Update driver.cpp

--- components/driver.cpp
+++ components/driver.cpp
@@ -1,5 +1,3 @@
-#pragma once
-
 #include <cstdlib>
 #include <iostream>
 #include <cstring>
@@ -9,6 +7,8 @@
 #include "Loader.hpp"
 #include "PCBManager.hpp"
 #include "Process.hpp"
+#include "LongTerm.hpp"
+#include "ShortTerm.hpp"
 #include "Types.hpp"
 
 using namespace std;
@@ -22,61 +22,26 @@ int main(int argc, char* argv[])
 	RAM ram;			// Random Access Memory
 	Disk disk;			// Simulated Disk
 
-	// TODO: Pass in queues to appropriate modules for initialization
-	queue<Process*> newQueue;		 // Initialized by Loader
-	queue<Process*> readyQueue;		 // Initialized by LongTermSched
-	queue<Process*> outputQueue;	 // Initialized by ShortTermSched
-
 	/*** Initialize other modules ***/
-	int cpuID = 1;
 	string jobFile = "JobFile.txt";
 
-	CPU cpu = CPU(&ram, cpuID);			     // CPU (id=1)
+	queue<Process*> newQueue;
+	queue<Process*> zeQueue;
+
+	CPU cpu = CPU(&ram, 1);			     // CPU (id=1)
 	Loader loader = Loader(&disk, &pcb, &newQueue);
-	//LongTerm...
-	
+	loader.LoadJobs(jobFile);			 // Load jobs into Disk
 
-	/*** Load processes into Disk ***/
-	loader.LoadJobs(jobFile);
-		/*
-		/*** simulate Long Term by moving first process to RAM 
-		Process* p = newQueue.front();
-		int ramAddress = 0;							// Load process in one at a time starting at 0 (only for now)
-		int originalProgBase = p->GetProgramBase();
-		instruction_t instr = disk.ReadInstruction(originalProgBase);
-		int newProgBase = ram.Allocate(instr, ramAddress);
+	LongTerm longTermSched(&newQueue, &zeQueue, &ram, &disk, &pcb);
 
-		for (int i = 1; i < p->GetFullProgramSize(); i++)
-		{
-			ramAddress += 4;
-			instr = disk.ReadInstruction(originalProgBase + i*4);
-			ram.Allocate(instr, ramAddress);
-		}
+	longTermSched.ScheduleJobs(FIFO);
 
-		p->SetProgramBase(newProgBase);
-		p->AssignInputBase(newProgBase + p->GetProgramSize());
-		p->AssignOutputBase(p->GetInputBase() + p->GetInputSize());
-		p->AssignTempBase(p->GetOutputBase() + p->GetOutputSize());
+	ShortTerm shortTermSched(&zeQueue, &longTermSched, &cpu);
 
-		p->SetState(READY);
-		readyQueue.push(p);
-		newQueue.pop();
-		*/
-	/***
-	This section mimics what the LongTerm, ShortTerm, and Dispatcher will accomplish once incorporated
-	***/
-
-	cout << "new Q size: " << newQueue.size() << endl;
-
-	while (!readyQueue.empty())
+	while ( longTermSched.FillZeQueue() == true )
 	{
-		Process* process = readyQueue.front();
-		cpu.RunProcess(process);
-		readyQueue.pop();
+		while ( shortTermSched.RunNextProcess() == true );
 	}
-
-	/*****************************/
-
 
 	return EXIT_SUCCESS;
 }
