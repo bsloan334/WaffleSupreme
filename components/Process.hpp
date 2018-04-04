36712e851837a181f23ebfc602847c577cf6ad7c
rbearden <rbeard13@students.kennesaw.edu>
2018-04-03 21:42:30 -0400

    Updated address and instruction size types to indicate whether it is byte or word address

--- components/Process.hpp
+++ components/Process.hpp
@@ -19,34 +19,17 @@ class Process
 {
     public:
         Process(int id, 
-                int instrSetSize, 
+                i_size_t instrSetSize, 
                 int priority, 
-                int inputSize, 
-                int outputSize, 
-                int tempSize) :
+                i_size_t inputSize, 
+                i_size_t outputSize, 
+                i_size_t tempSize) :
                 job(id, instrSetSize, priority),
                 data(inputSize, outputSize, tempSize)
         {
             registers = new instruction_t[NBR_OF_REGS]; // create empty register array
             currentState = NEW;
-            programCounter = new int(0);
-		}
-	
-		Process(const Process &p)
-		{
-			currentState = p.currentState;
-			programCounter = new int(*(p.programCounter));
-			registers = new instruction_t[NBR_OF_REGS];
-			programBase = p.programBase;
-			inputBase = p.inputBase;
-			outputBase = p.outputBase;
-			tempBase = p.tempBase;
-			job = Job(p.job);
-			data = Data(p.data);
-			fullProgramSize = p.fullProgramSize;
-
-			for (int i = 0; i < NBR_OF_REGS; i++)
-				*(registers + i) = *(p.registers + i);
+            programCounter = new b_address_t(0);
 		}
 
 		~Process() 
@@ -71,43 +54,43 @@ class Process
     
         // Provides CPU direct read/write access to Registers and PC
         instruction_t* Registers() { return registers; }
-        int* ProgramCounter() { return programCounter; }
+        b_address_t* ProgramCounter() { return programCounter; }
 	
         // Set locations in Disk/RAM of different program sections
-        int GetProgramBase() { return programBase; }
-		int GetProgramEnd() { return programBase + fullProgramSize; }
-        int GetInputBase() { return inputBase; }
-        int GetOutputBase() { return outputBase; }
-        int GetTempBase() { return tempBase; }
+        b_address_t GetProgramBase() { return programBase; }
+		b_address_t GetProgramEnd() { return programBase + WORD*GetFullProgramSize(); }
+        b_address_t GetInputBase() { return programBase + WORD*GetProgramSize(); }
+        b_address_t GetOutputBase() { return GetInputBase() + WORD*GetInputSize(); }
+        b_address_t GetTempBase() { return GetOutputBase() + WORD*GetOutputSize(); }
 
 		// Get fields from Job and Data
-		size_t GetProgramSize() { return job.GetProgramSize(); }
-		size_t GetInputSize() { return data.GetInputBufferSize(); }
-		size_t GetOutputSize() { return data.GetOutputBufferSize(); }
-		size_t GetTempSize() { return data.GetTempBufferSize(); }
-		size_t GetFullProgramSize() { return job.GetProgramSize() +
-					data.GetInputBufferSize() +
-					data.GetOutputBufferSize() +
-					data.GetTempBufferSize(); }
+		i_size_t GetProgramSize() { return job.GetProgramSize(); }
+		i_size_t GetInputSize() { return data.GetInputBufferSize(); }
+		i_size_t GetOutputSize() { return data.GetOutputBufferSize(); }
+		i_size_t GetTempSize() { return data.GetTempBufferSize(); }
+		i_size_t GetFullProgramSize() {
+			return GetProgramSize() + GetInputSize() + GetOutputSize() + GetTempSize();
+		}
 		
 		// Other process properties
 		int GetID() { return job.GetProgramID(); }
 		int GetPriority() { return job.GetPriority(); }
 
-        void SetProgramBase(int address) { programBase = address; }
-        void AssignOutputBase(int oBase) { outputBase = oBase; }
-        void AssignTempBase(int tBase) { tempBase = tBase; }
-        void AssignInputBase(int iBase) { inputBase = iBase; }
-		void SetFullProgramSize(size_t size) { fullProgramSize = size; }
+        void SetProgramBase(b_address_t address) { programBase = address; }
+        //void AssignOutputBase(int oBase) { outputBase = oBase; }
+        //void AssignTempBase(int tBase) { tempBase = tBase; }
+        //void AssignInputBase(int iBase) { inputBase = iBase; }
+		//void SetFullProgramSize(size_t size) { fullProgramSize = size; }
 
 		// Mass refactor functions
+		/*
 		void RefactorSectionBases()
 		{
 			inputBase = programBase + job.GetProgramSize();
 			outputBase = inputBase + data.GetInputBufferSize();
 			tempBase = outputBase + data.GetOutputBufferSize();
 		}
-
+		*/
 
 
     private:
@@ -117,12 +100,15 @@ class Process
         Data data;
     
         int currentState;                             // By default, state is NEW
-        int* programCounter;                          // Integer offset from programBase
+        b_address_t* programCounter;                          // Integer offset from programBase
     
-        int programBase = 0;                        // Absolute address in RAM of program start
-        int inputBase = 0;                          // Offset from programBase
+		
+        b_address_t programBase = 0;                        // Absolute address in RAM of program start
+		/*
+		int inputBase = 0;                          // Offset from programBase
         int outputBase = 0;                         // Offset from programBase
         int tempBase = 0;                           // Offset from programBase
+		*/
 
-		size_t fullProgramSize = 0;
+		//size_t fullProgramSize = 0;
 };
