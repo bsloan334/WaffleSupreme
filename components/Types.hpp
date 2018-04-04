3fa785765bf8edb2621d1e61dd421e77aacd74f5
rbearden <rbeard13@students.kennesaw.edu>
2018-04-03 21:33:51 -0400

    Update Types.hpp

--- components/Types.hpp
+++ components/Types.hpp
@@ -2,8 +2,13 @@
 #include <stdint.h>
 
 #define WORD 4
+#define NULL_ADDRESS -1
 
 typedef uint32_t instruction_t;
 typedef uint8_t byte_t;
 
+typedef int i_address_t;
+typedef int b_address_t;
 
+typedef size_t i_size_t;
+typedef size_t b_size_t;
