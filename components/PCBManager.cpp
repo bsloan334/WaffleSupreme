c9549dfd77efa04c7cabace97d46e56931f854d7
rbearden <rbeard13@students.kennesaw.edu>
2018-04-11 19:24:21 -0400

    Update PCBManager.cpp

--- components/PCBManager.cpp
+++ components/PCBManager.cpp
@@ -4,50 +4,30 @@
 
 using namespace std;
 
-Process* PCBManager::GetPCBHead()
-{
-	listHead = &PCB.front();
-
-	return listHead;
-}
-
-void PCBManager::AddProcess(Process newProcess)
+void PCBManager::AddProcess(Process* newProcess)
 {
 	PCB.push_back(newProcess);
-	size++;
 }
 
-void PCBManager::RemoveProcess(Process p)
+void PCBManager::RemoveProcess(Process* p)
 {
-	if (&p == listHead)
-	{
-		PCB.remove(p);
-		listHead = &PCB.front();
-	}
-	else
-	{
-		PCB.remove(p);
-	}
-
-	size = PCB.size();
+	PCB.remove(p);
 }
 
 int PCBManager::GetSize()
 {
-	return size;
+	return PCB.size();
 }
 
 Process* PCBManager::FindProcess(int index)
 {
 
-	if (size == 0)
+	if (GetSize() == 0)
 		return NULL;
 
-	list<Process>::const_iterator itr = PCB.cbegin();
-	for (int i = 0; i < size; i++)
+	list<Process*>::iterator itr = PCB.begin();
+	for (int i = 0; i < GetSize(); i++)
 		itr++;
 
-	Process p = *itr;
-
-	return &p;
+	return *itr;
 }
